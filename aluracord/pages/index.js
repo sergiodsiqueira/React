dece501a7a0e0adc8eb344df64fbf2780ffb7d2e
Sérgio de Siqueira Silva <sergiodsiqueira@yahoo.com.br>
2022-01-31 14:16:13 -0300

    Update 2.1
    Aplicação da aula 2 da imersão react da Alura.

--- aluracord/pages/index.js
+++ aluracord/pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Text, TextField, Image, Button, Icon } from '@skynexui/components';
+import { useState } from 'react';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+import { useRouter } from 'next/router';
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -47,11 +20,11 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'sergiodsiqueira';
+  const [username, setUsername] = useState('')
+  const routers = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -77,6 +50,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(props)=>{
+              props.preventDefault();
+              routers.push('chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -89,11 +66,14 @@ export default function PaginaInicial() {
             
             <Titulo tag="h2">Minimal Talks</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[700] }}>
-              Welcome
+              Bem-vindo
             </Text>
 
             <TextField
               fullWidth
+              value={username}
+              onChange={(event)=>{const valor = event.target.value;
+                                  setUsername(valor)}}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -106,10 +86,10 @@ export default function PaginaInicial() {
             
             <Button //Botão de Login
               type='submit'
-              label='CONTINUE'
+              label='Entrar'
               fullWidth
               buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
+                contrastColor: appConfig.theme.colors.neutrals[900],
                 mainColor: appConfig.theme.colors.primary[500],
                 mainColorLight: appConfig.theme.colors.primary[400],
                 mainColorStrong: appConfig.theme.colors.primary[600],
