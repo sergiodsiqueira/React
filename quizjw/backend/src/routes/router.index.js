bb83c8ca61bd72ef69751e136c85a50530b83ce3
Sérgio de Siqueira Silva <sergiodsiqueira@yahoo.com.br>
2023-03-20 15:20:15 -0300

    Update router.index.js
    Update dataHora for const

--- quizjw/backend/src/routes/router.index.js
+++ quizjw/backend/src/routes/router.index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-var dataHora = new Date();
+const dataHora = new Date();
 router.get("/", (req, res) => {
   res.status(200).send({
     status: "OK",
